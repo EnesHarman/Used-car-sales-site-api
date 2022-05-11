dd36e0c6a1c4f4d9ca83a35dbf6e9b6467c78969
EnesHarman <enesharman60@gmail.com>
2022-05-11 16:33:56 +0300

    Initial Commit

---
+++ src/main/java/com/sahibinden/arac/AracApplication.java
@@ -0,0 +1,13 @@
+package com.sahibinden.arac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahibinden/arac/AracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahibinden.arac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
