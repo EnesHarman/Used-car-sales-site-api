b75441b8c9658062bacf2ca7a9651c748448bdd3
EnesHarman <enesharman60@gmail.com>
2022-05-12 10:57:50 +0300

    Add result classes

---
+++ src/main/java/com/sahibinden/arac/core/result/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.sahibinden.arac.core.result;
+
+public class ErrorResult extends Result{
+    public ErrorResult() {
+        super(false);
+    }
+
+    public ErrorResult( String message) {
+        super(false, message);
+    }
+}
---
+++ src/main/java/com/sahibinden/arac/core/result/Result.java
@@ -0,0 +1,24 @@
+package com.sahibinden.arac.core.result;
+
+public abstract class Result {
+    private boolean success;
+    private String message;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
+
+    public Result(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
